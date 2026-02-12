9c7ccd212cdf4c570da6bb4cdacd3f493baa52e4
mustang54200 <theo.foreau1411@gmail.com>
2026-02-12 17:48:25 +0100

    Generations update pt.4

--- src/main/java/fr/ul/miage/generation/Generateur.java
+++ src/main/java/fr/ul/miage/generation/Generateur.java
@@ -7,24 +7,30 @@ import fr.ul.miage.tds.Tds;
 import java.util.List;
 
 public class Generateur {
+    private Tds tableDesSymboles;
 
-    public String genererProgramme(Noeud pgr, Tds tableDesSymboles) {
+    public Generateur(Tds tds) {
+        this.tableDesSymboles = tds;
+    }
+
+    public String genererProgramme(Noeud pgr) {
         StringBuffer code = new StringBuffer();
 
-        code.append(".include beta.uasm\n");
-        code.append(".include intio.uasm\n");
+        code.append(".include beta.uasm\n")
+            .append(".include intio.uasm\n");
 
-        code.append("CMOVE(pile, SP\n");
-        code.append("BR(debut)\n");
-        code.append("debut: ");
+        code.append("\tCMOVE(pile, SP\n")
+            .append("\tBR(debut)\n")
+            .append("debut: ");
 
-        genererData(tableDesSymboles);
+        code.append(genererData());
 
-        code.append("CALL(main)\nHALT\n");
+        code.append("\TCALL(main)\n")
+            .append("\tHALT\n");
 
         for (Noeud func: pgr.getFils()) {
             if (func instanceof Fonction) {
-                code.append(genererFonction((Fonction) func, tableDesSymboles));
+                code.append(genererFonction((Fonction) func));
             }
         }
         code.append("pile:\n");
@@ -32,69 +38,78 @@ public class Generateur {
         return code.toString();
     }
 
-    public String genererInstruction(Noeud n, Tds tableDesSymboles) {
+    public String genererInstruction(Noeud n) {
         StringBuffer code = new StringBuffer();
 
         switch (n.getCat()) {
             case AFF: code.append(genererAffectation((Affectation) n.getFils().get(0)));
-            case SI: code.append(genererSi((Si) n.getFils().get(0), tableDesSymboles));
-            case TQ: code.append(genererTantQue(n.getFils().get(0), tableDesSymboles));
-            case ECR: code.append(genererEcriture(n.getFils().get(0)));
-            case APPEL: code.append(genererAppel((Fonction) n.getFils(), tableDesSymboles));
+            case SI: code.append(genererSi((Si) n.getFils().get(0)));
+            case TQ: code.append(genererTantQue((TantQue) n.getFils().get(0)));
+            case ECR: code.append(genererEcriture((Ecrire) n.getFils().get(0)));
+            case APPEL: code.append(genererAppel((Fonction) n.getFils()));
+            case RET: code.append(genererRetour(Retour) n.getFils().get(0));
         }
 
         return code.toString();
     }
 
-    public String genererFonction(Noeud fonct, Symbole tableDesSymboles) {
+    public String genererFonction(Noeud fonct) {
         StringBuffer code = new StringBuffer();
         code.append(fonct.toString());
 
         code.append("\tPUSH(LP)\n");
         code.append("\tPUSH(BP)\n");
         code.append("\tMOVE(SP,BP)\n");
-        code.append("\tALLOCATE(" + tableDesSymboles.getNbVar() + "\n"); // MARCHE PAS CAR IL CONNAIT PAS LE TDS
+        code.append("\tALLOCATE(").append(this.tableDesSymboles.getSymboleFromName(fonct.toString()).getNbVar()).append("\n"); // MARCHE PAS CAR IL CONNAIT PAS LE TDS
 
         for(Noeud f: fonct.getFils()) {
-            code.append(genererInstruction(((Fonction) f, tableDesSymboles)); // MARCHE PAS CAR IL CONNAIT PAS GENERER INSTRUCTION
+            code.append(genererInstruction((Fonction) f)); // MARCHE PAS CAR IL CONNAIT PAS GENERER INSTRUCTION
         }
 
-        code.append("\tDEALLOCATE(" + tableDesSymboles.getNbVar() + "\n"); // MARCHE PAS CAR IL CONNAIT PAS LE TDS (PARTIE 2)
-        code.append("\tPOP(BP)\n");
-        code.append("\tPOP(LP)\n");
-        code.append("\tRTN()\n");
+        code.append("\tDEALLOCATE(").append(this.tableDesSymboles.getSymboleFromName(fonct.toString()).getNbVar()).append("\n") // MARCHE PAS CAR IL CONNAIT PAS LE TDS (PARTIE 2)
+            .append("\tPOP(BP)\n")
+            .append("\tPOP(LP)\n")
+            .append("\tRTN()\n");
 
         return code.toString();
     }
 
-    public String genererRetour(Noeud arbre, Tds tableDesSymboles) {
+    public String genererRetour(Retour arbre) {
         StringBuffer code = new StringBuffer();
+        code.append(genererExpression(arbre.getLeFils()));
 
-        code.append(genererExpression(arbre.getFils().get(0)));
+        Symbole symboleRetourArbre = this.tableDesSymboles.getSymboleFromName(arbre.toString());
+        int offset = 2 + symboleRetourArbre.getNbParam();
 
-        int offset = 2 + tableDesSymboles. // A FINIR
+        code.append("\tPOP(R0)\n")
+            .append("\tPUTFRAME(R0").append(offset * 4).append("\n")
+            .append("\t").append("ret_").append(arbre.toString()).append("\n");
+
+        return code.toString();
     }
 
-    public String genererSi(Noeud arbre, Tds tableDesSymboles) {
+    public String genererSi(Noeud arbre) {
         StringBuffer code = new StringBuffer();
         List<Noeud> enfantsArbre = arbre.getFils();
 
-        code.append(arbre.toString());
-        code.append(genererCondition(enfantsArbre.get(0)));
+        code.append(arbre.toString())
+            .append(genererCondition(enfantsArbre.get(0)));
 
-        code.append("\tPOP(R0)\n");
-        code.append("\tBF(RO,").append(arbre.toString()).append("\n"); // "sinon_a.valeur"
+        code.append("\tPOP(R0)\n")
+            .append("\tBF(RO,").append(arbre.toString()).append("\n"); // "sinon_a.valeur"
 
-        code.append(genererBloc(enfantsArbre.get(1), tableDesSymboles) + "\n");
+        code.append(genererBloc(enfantsArbre.get(1)));
 
-        code.append("\tBR(fsi," + arbre.toString() + "\n");  //
-        code.append(arbre.toString()).append(":\n"); // "sinon_a.valeur:"
-        code.append(genererBloc(enfantsArbre.get(2), tableDesSymboles));
+        code.append("\tBR(fsi,").append(arbre.toString()).append("\n")  //
+            .append(arbre.toString()).append(":\n") // "sinon_a.valeur:"
+            .append(genererBloc(enfantsArbre.get(2)));
 
         code.append(arbre.toString()).append(":\n"); // "fsi_a.valeur:"
+
+        return code.toString();
     }
 
-    public String genererTantQue(Noeud arbre, Tds tableDesSymboles) {
+    public String genererTantQue(Noeud arbre) {
         StringBuffer code = new StringBuffer();
 
         code.append(arbre.toString()).append(":\n");
@@ -102,15 +117,15 @@ public class Generateur {
 
         code.append("\tBF(R0").append(arbre.toString()).append(")\n"); // "ftq_a.valeur"
 
-        code.append(genererBloc(arbre.getFils().get(1), tableDesSymboles));
+        code.append(genererBloc(arbre.getFils().get(1)));
 
     }
 
-    public String genererBloc(Noeud arbre, Tds tableDesSymboles) {
+    public String genererBloc(Noeud arbre) {
         StringBuffer code = new StringBuffer();
 
         for(Noeud f: arbre.getFils()) {
-            code.append(genererInstruction((Fonction) f, tableDesSymboles));
+            code.append(genererInstruction((Fonction) f));
         }
 
         return code.toString();
@@ -118,10 +133,13 @@ public class Generateur {
 
     public String genererAffectation(Affectation aff) {
         StringBuffer code = new StringBuffer();
-        code.append(genererExpression(aff.getFilsDroit()));
-        code.append("\tPOP(R0)\n");
+
+        code.append(genererExpression(aff.getFilsDroit()))
+            .append("\tPOP(R0)\n");
+
         Idf var = (Idf) aff.getFilsGauche();
-        code.append("\tST(R0, " + var.getValeur() + ")\n");
+
+        code.append("\tST(R0, ").append(var.getValeur()).append(")\n");
         return code.toString();
     }
 
@@ -130,56 +148,55 @@ public class Generateur {
 
         switch(arbre.getCat()) {
             case SUP:
-                code.append(genererExpression(arbre.getFils().get(0)));
-                code.append(genererExpression(arbre.getFils().get(1)));
+                code.append(genererExpression(arbre.getFils().get(0)))
+                    .append(genererExpression(arbre.getFils().get(1)));
 
-                code.append("\tPOP(R1)\n");
-                code.append("\tPOP(R2)\n");
-                code.append("\tCMPLT(R2,R1,R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R1)\n")
+                    .append("\tPOP(R2)\n")
+                    .append("\tCMPLT(R2,R1,R3)\n")
+                    .append("\tPUSH(R3)\n");
                 break;
 
             case SUPE:
-                code.append(genererExpression(arbre.getFils().get(0)));
-                code.append(genererExpression(arbre.getFils().get(1)));
+                code.append(genererExpression(arbre.getFils().get(0)))
+                    .append(genererExpression(arbre.getFils().get(1)));
 
-                code.append("\tPOP(R1)\n");
-                code.append("\tPOP(R2)\n");
-                code.append("\tCMPLE(R2,R1,R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R1)\n")
+                    .append("\tPOP(R2)\n")
+                    .append("\tCMPLE(R2,R1,R3)\n")
+                    .append("\tPUSH(R3)\n");
                 break;
 
             case INF:
-                code.append(genererExpression(arbre.getFils().get(0)));
-                code.append(genererExpression(arbre.getFils().get(1)));
+                code.append(genererExpression(arbre.getFils().get(0)))
+                    .append(genererExpression(arbre.getFils().get(1)));
 
-                code.append("\tPOP(R1)\n");
-                code.append("\tPOP(R2)\n");
-                code.append("\tCMPL(R1,R2,R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R1)\n")
+                    .append("\tPOP(R2)\n")
+                    .append("\tCMPL(R1,R2,R3)\n")
+                    .append("\tPUSH(R3)\n");
                 break;
 
             case INFE:
-                code.append(genererExpression(arbre.getFils().get(0)));
-                code.append(genererExpression(arbre.getFils().get(1)));
+                code.append(genererExpression(arbre.getFils().get(0)))
+                    .append(genererExpression(arbre.getFils().get(1)));
 
-                code.append("\tPOP(R1)\n");
-                code.append("\tPOP(R2)\n");
-                code.append("\tCMPLT(R1,R2,R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R1)\n")
+                    .append("\tPOP(R2)\n")
+                    .append("\tCMPLT(R1,R2,R3)\n")
+                    .append("\tPUSH(R3)\n");
                 break;
 
             case EG:
-                code.append(genererExpression(arbre.getFils().get(0)));
-                code.append(genererExpression(arbre.getFils().get(1)));
+                code.append(genererExpression(arbre.getFils().get(0)))
+                    .append(genererExpression(arbre.getFils().get(1)));
 
-                code.append("\tPOP(R1)\n");
-                code.append("\tPOP(R2)\n");
-                code.append("\tCMPEQ(R1,R2,R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R1)\n")
+                    .append("\tPOP(R2)\n")
+                    .append("\tCMPEQ(R1,R2,R3)\n")
+                    .append("\tPUSH(R3)\n");
 
         }
-
         return code.toString();
     }
 
@@ -190,17 +207,15 @@ public class Generateur {
             case CONST:
                 Const c = (Const) expr;
 
-                code.append("\tCMOVE(R0, " + c.getValeur() + ")\n");
-                code.append("\tPUSH(R0)\n");
-
+                code.append("\tCMOVE(R0, ").append(c.getValeur()).append(")\n")
+                    .append("\tPUSH(R0)\n");
                 break;
 
             case IDF:
                 Idf i = (Idf) expr;
 
-                code.append("\tCMOVE(R0, " + i.getValeur() + ")\n");
-                code.append("\tPUSH(R0)\n");
-
+                code.append("\tCMOVE(R0, ").append(i.getValeur()).append(")\n")
+                    .append("\tPUSH(R0)\n");
                 break;
 
             case PLUS:
@@ -208,13 +223,13 @@ public class Generateur {
                 Noeud gaucheP = p.getFilsGauche();
                 Noeud droitP = p.getFilsDroit();
 
-                code.append(genererExpression(gaucheP));
-                code.append(genererExpression(droitP));
+                code.append(genererExpression(gaucheP))
+                    .append(genererExpression(droitP));
 
-                code.append("\tPOP(R2)\n");
-                code.append("\tPOP(R1)\n");
-                code.append("\tADD(R1, R2, R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R2)\n")
+                    .append("\tPOP(R1)\n")
+                    .append("\tADD(R1, R2, R3)\n")
+                    .append("\tPUSH(R3)\n");
                 break;
 
             case MOINS:
@@ -222,13 +237,13 @@ public class Generateur {
                 Noeud gaucheM = m.getFilsGauche();
                 Noeud droitM = m.getFilsDroit();
 
-                code.append(genererExpression(gaucheM));
-                code.append(genererExpression(droitM));
+                code.append(genererExpression(gaucheM))
+                    .append(genererExpression(droitM));
 
-                code.append("\tPOP(R2)\n");
-                code.append("\tPOP(R1)\n");
-                code.append("\tSUB(R1, R2, R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R2)\n")
+                    .append("\tPOP(R1)\n")
+                    .append("\tSUB(R1, R2, R3)\n")
+                    .append("\tPUSH(R3)\n");
                 break;
 
             case MUL:
@@ -236,13 +251,13 @@ public class Generateur {
                 Noeud gaucheMul = mul.getFilsGauche();
                 Noeud droitMul = mul.getFilsDroit();
 
-                code.append(genererExpression(gaucheMul));
-                code.append(genererExpression(droitMul));
+                code.append(genererExpression(gaucheMul))
+                    .append(genererExpression(droitMul));
 
-                code.append("\tPOP(R2)\n");
-                code.append("\tPOP(R1)\n");
-                code.append("\tMUL(R1, R2, R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R2)\n")
+                    .append("\tPOP(R1)\n")
+                    .append("\tMUL(R1, R2, R3)\n")
+                    .append("\tPUSH(R3)\n");
                 break;
 
             case DIV:
@@ -250,13 +265,13 @@ public class Generateur {
                 Noeud gaucheD = div.getFilsGauche();
                 Noeud droitD = div.getFilsDroit();
 
-                code.append(genererExpression(gaucheD));
-                code.append(genererExpression(droitD));
+                code.append(genererExpression(gaucheD))
+                    .append(genererExpression(droitD));
 
-                code.append("\tPOP(R2)\n");
-                code.append("\tPOP(R1)\n");
-                code.append("\tDIV(R1, R2, R3)\n");
-                code.append("\tPUSH(R3)\n");
+                code.append("\tPOP(R2)\n")
+                    .append("\tPOP(R1)\n")
+                    .append("\tDIV(R1, R2, R3)\n")
+                    .append("\tPUSH(R3)\n");
                 break;
 
             default:
@@ -265,34 +280,42 @@ public class Generateur {
         return code.toString();
     }
 
-    public String genererData(Tds tds){
-        StringBuffer code=new StringBuffer();
-        code.append("");
-        for (Symbole e : tds.getTable()){
-            if(e.getType()== Symbole.Type.entier && e.getCat()== Symbole.Categorie.global){
-                code.append(e.getNom());
-                code.append(":LONG()"+e.getVal());
+    public String genererData(){
+        StringBuffer code = new StringBuffer();
+
+        for (Symbole e : this.tableDesSymboles.getTable()){
+            if(e.getType().equals(Symbole.Type.entier)
+                    && e.getCat().equals(Symbole.Categorie.global)){
+
+                code.append(e.getNom()).append(":")
+                    .append("LONG(").append(e.getVal()).append(")\n");
             }
         }
         return code.toString();
     }
-    public String genererEcriture(Noeud a){
-        StringBuffer code=new StringBuffer();
-        code.append(genererExpression(a.getFils(0)));
-        code.append("\tPOP(R0)\nWRINT()");
+
+    public String genererEcriture(Ecrire arbre){
+        StringBuffer code = new StringBuffer();
+
+        code.append(genererExpression(arbre.getLeFils()))
+            .append("\tPOP(R0)\nWRINT()");
+
         return code.toString();
     }
 
-    public String genererAppel(Fonction a,Tds tableDesSymboles){
-        StringBuffer code=new StringBuffer();
-        if(a.getValeur()!=null){
+    public String genererAppel(Fonction a){
+        StringBuffer code = new StringBuffer();
+
+        if(a.getValeur() != null){
             code.append("ALLOCATE(1)");
         }
+
         for(Noeud f: a.getFils()){
             code.append(genererExpression(f));
         }
-        code.append("CALL("+a.getValeur()+")");
-        code.append("DEALLOCATE("+tableDesSymboles.getNbParam()+")");
+
+        code.append("CALL(").append(a.getValeur()).append(")")
+            .append("DEALLOCATE("+ this.tableDesSymboles.getSymboleFromName(a.toString()).getNbParam()+")");
         return code.toString();
     }
 }
--- src/main/java/fr/ul/miage/tds/Tds.java
+++ src/main/java/fr/ul/miage/tds/Tds.java
@@ -4,9 +4,21 @@ package fr.ul.miage.tds;
 import java.util.ArrayList;
 
 public class Tds {
-    private final ArrayList<Symbole> table=new ArrayList<>();
+    private final ArrayList<Symbole> table = new ArrayList<>();
 
     public ArrayList<Symbole> getTable(){
         return  this.table;
+    }
+
+    public Symbole getSymboleFromName(String name){
+        /*
+        A partir du nom du noeud, on peut récuperer le symbole associé via son nom
+         */
+        for(Symbole S: this.getTable()) {
+            if(S.getNom().equals(name)) {
+                return S;
+            }
+        }
+        return null;
     }
 }
